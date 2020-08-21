3eed02fae1c2e218b3c25efeb0c6322de554a60d
knut0297 <knut0297@umn.edu>
2020-08-21 15:41:00 -0500

    Add python modules for standalone ver 2.7.15

---
+++ scipy_1.2.2_python2.7.15_install_notes.sh
@@ -0,0 +1,117 @@
+#!/bin/bash
+
+# 2019-11-13
+
+
+#######################################################################
+# 
+#######################################################################
+
+
+
+MODULE_NAME=scipy
+VERSION=1.2.2_python2.7.15
+MODULES_DIR=$HOME/software/modules
+MODULESFILES_DIR=$HOME/software/modulesfiles
+
+
+
+#######################################################################
+# Install in modules dir
+#######################################################################
+
+
+mkdir -p $MODULES_DIR/$MODULE_NAME/$VERSION
+cd $MODULES_DIR/$MODULE_NAME/$VERSION
+
+
+
+module purge
+module load python2/2.7.15
+
+# Set up a folder for all python stuff called "build"
+mkdir -p $MODULES_DIR/$MODULE_NAME/$VERSION/build
+
+
+PYTHONUSERBASE=$MODULES_DIR/$MODULE_NAME/$VERSION/build pip install --user --ignore-installed scipy==1.2.2
+
+
+
+
+
+# ---------------------------------------------------------------------
+# Create the modulefile
+# ---------------------------------------------------------------------
+
+mkdir -p $MODULESFILES_DIR/$MODULE_NAME
+cd $MODULESFILES_DIR/$MODULE_NAME
+
+
+
+
+cat > $VERSION <<ENDOFMESSAGE
+#%Module######################################################################
+
+
+# Create a help message for the module
+# e.g. 
+# module help <module_name>
+
+proc ModulesHelp { } {
+        global version
+        puts stderr "\tThis module adds $MODULE_NAME $VERSION executable to your path."
+}
+
+
+# Update the necessary shell environment variables to make the software work
+module load python2/2.7.15
+prepend-path PATH "$MODULES_DIR/$MODULE_NAME/$VERSION/build/bin"
+prepend-path PYTHONPATH "$MODULES_DIR/$MODULE_NAME/$VERSION/build/lib/python2.7/site-packages"
+
+
+ENDOFMESSAGE
+
+
+
+
+
+# ---------------------------------------------------------------------
+# Create the modulefile default version
+# ---------------------------------------------------------------------
+
+
+# Set the version
+cat > .version <<ENDOFMESSAGE
+#%Module
+set ModulesVersion "$VERSION"
+
+ENDOFMESSAGE
+
+
+
+
+
+
+# ---------------------------------------------------------------------
+# Update permissions (if you want to share the module)
+# ---------------------------------------------------------------------
+
+
+# Make all directories readable and executable
+find $MODULES_DIR/$MODULE_NAME -type d -print0 | xargs -0 chmod a+rxs
+find $MODULESFILES_DIR/$MODULE_NAME -type d -print0 | xargs -0 chmod a+rxs
+
+# Make all files readable
+find $MODULES_DIR/$MODULE_NAME -type f -print0 | xargs -0 chmod a+r
+find $MODULESFILES_DIR/$MODULE_NAME -type f -print0 | xargs -0 chmod a+r
+
+# Make all files, that are already executable, readable and executable
+find $MODULES_DIR/$MODULE_NAME -type f -executable -print0 | xargs -0 chmod a+rx
+# Note: there are no executable files in the modulesfiles directory
+
+
+
+
+
+
+
