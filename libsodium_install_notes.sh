240ea6bb803806fda944cc03e4cd00d12c739ee0
knut0297 <knut0297@umn.edu>
2020-10-08 14:37:42 -0500

    libsodium library for R package sodium

---
+++ libsodium_install_notes.sh
@@ -0,0 +1,128 @@
+#!/bin/bash
+
+# 2020-10-08
+
+
+#######################################################################
+# 
+#######################################################################
+
+# https://doc.libsodium.org/installation
+
+# This library is needed for the R package "sodium"
+# https://cran.r-project.org/web/packages/sodium/index.html
+
+
+MODULE_NAME=libsodium
+VERSION=1.0.18
+MODULES_DIR=$HOME/software/modules
+MODULESFILES_DIR=$HOME/software/modulesfiles
+
+
+
+#######################################################################
+# Install in modules dir
+#######################################################################
+
+
+mkdir -p $MODULES_DIR/$MODULE_NAME/$VERSION
+cd $MODULES_DIR/$MODULE_NAME/$VERSION
+
+
+
+# Get the binary for linux
+wget https://download.libsodium.org/libsodium/releases/libsodium-1.0.18.tar.gz
+
+tar xvzf libsodium-1.0.18.tar.gz
+cd libsodium-1.0.18
+
+module purge
+module load gcc/7.2.0
+
+./configure --prefix $MODULES_DIR/$MODULE_NAME/$VERSION
+
+make
+make check
+make install
+
+
+
+
+
+# ---------------------------------------------------------------------
+# Create the modulefile
+# ---------------------------------------------------------------------
+
+mkdir -p $MODULESFILES_DIR/$MODULE_NAME
+cd $MODULESFILES_DIR/$MODULE_NAME
+
+
+
+
+cat > $VERSION <<EOF
+#%Module######################################################################
+
+
+# Create a help message for the module
+# e.g. 
+# module help <module_name>
+
+proc ModulesHelp { } {
+        global version
+        puts stderr "\tThis module adds $MODULE_NAME $VERSION executable to your path."
+}
+
+
+# Update the necessary shell environment variables to make the software work
+prepend-path INCLUDE $MODULES_DIR/$MODULE_NAME/$VERSION/include
+prepend-path CPATH $MODULES_DIR/$MODULE_NAME/$VERSION/include
+prepend-path C_INCLUDE_PATH $MODULES_DIR/$MODULE_NAME/$VERSION/include
+prepend-path CPLUS_INCLUDE_PATH $MODULES_DIR/$MODULE_NAME/$VERSION/include
+prepend-path FPATH $MODULES_DIR/$MODULE_NAME/$VERSION/include
+prepend-path LD_LIBRARY_PATH $MODULES_DIR/$MODULE_NAME/$VERSION/lib
+prepend-path LD_RUN_PATH $MODULES_DIR/$MODULE_NAME/$VERSION/lib
+prepend-path LIBRARY_PATH $MODULES_DIR/$MODULE_NAME/$VERSION/lib
+prepend-path PKG_CONFIG_PATH $MODULES_DIR/$MODULE_NAME/$VERSION/lib/pkgconfig
+
+EOF
+
+
+
+
+
+# ---------------------------------------------------------------------
+# Create the modulefile default version
+# ---------------------------------------------------------------------
+
+
+# Set the version
+cat > .version <<EOF
+#%Module
+set ModulesVersion "$VERSION"
+
+EOF
+
+
+
+
+
+
+# ---------------------------------------------------------------------
+# Update permissions (if you want to share the module)
+# ---------------------------------------------------------------------
+
+
+# Make all directories readable and executable
+find $MODULES_DIR/$MODULE_NAME -type d -print0 | xargs -0 chmod a+rxs
+find $MODULESFILES_DIR/$MODULE_NAME -type d -print0 | xargs -0 chmod a+rxs
+
+# Make all files readable
+find $MODULES_DIR/$MODULE_NAME -type f -print0 | xargs -0 chmod a+r
+find $MODULESFILES_DIR/$MODULE_NAME -type f -print0 | xargs -0 chmod a+r
+
+# Make all files, that are already executable, readable and executable
+find $MODULES_DIR/$MODULE_NAME -type f -executable -print0 | xargs -0 chmod a+rx
+# Note: there are no executable files in the modulesfiles directory
+
+
+
