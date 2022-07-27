4abbacc44204f4375016012777fd04d1e94c6e60
knut0297 <knut0297@umn.edu>
2022-07-27 16:27:20 -0500

    Add libevent

---
+++ libevent_2.1.12_install_notes.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+
+# 2022-07-27
+
+
+#######################################################################
+# NOTE:
+#######################################################################
+
+
+
+MODULE_NAME=libevent
+VERSION=2.1.12
+MODULES_DIR=$HOME/software/modules
+MODULESFILES_DIR=$HOME/software/modulesfiles
+
+
+
+#######################################################################
+# Install in modules dir
+#######################################################################
+
+
+mkdir -p $MODULES_DIR/$MODULE_NAME/$VERSION/build
+cd $MODULES_DIR/$MODULE_NAME/$VERSION
+
+
+# Download
+wget https://github.com/libevent/libevent/releases/download/release-${VERSION}-stable/libevent-${VERSION}-stable.tar.gz
+
+tar xzvf libevent-${VERSION}-stable.tar.gz
+cd libevent-${VERSION}-stable
+
+module load gcc/7.2.0
+
+./configure --prefix=$MODULES_DIR/$MODULE_NAME/$VERSION/build --enable-shared
+make -j 24
+make install
+
+
+
+# ---------------------------------------------------------------------
+# Create the modulefile
+# ---------------------------------------------------------------------
+
+mkdir -p $MODULESFILES_DIR/$MODULE_NAME
+cd $MODULESFILES_DIR/$MODULE_NAME
+
+
+
+
+cat > $VERSION <<EOF
+#%Module######################################################################
+
+prepend-path PATH $MODULES_DIR/$MODULE_NAME/$VERSION/build/bin
+prepend-path LIBRARY_PATH $MODULES_DIR/$MODULE_NAME/$VERSION/build/lib
+prepend-path LD_LIBRARY_PATH $MODULES_DIR/$MODULE_NAME/$VERSION/build/lib
+prepend-path C_INCLUDE_PATH $MODULES_DIR/$MODULE_NAME/$VERSION/build/include
+prepend-path CPLUS_INCLUDE_PATH $MODULES_DIR/$MODULE_NAME/$VERSION/build/include
+prepend-path PKG_CONFIG_PATH $MODULES_DIR/$MODULE_NAME/$VERSION/build/lib/pkgconfig
+
+EOF
+
+
+
+
+
+# ---------------------------------------------------------------------
+# Create the modulefile default version
+# ---------------------------------------------------------------------
+
+
+# Set the version
+cat > .version <<EOF
+#%Module
+set ModulesVersion "$VERSION"
+
+EOF
+
+
+
+
+
+
+# ---------------------------------------------------------------------
+# Update permissions (if you want to share the module)
+# ---------------------------------------------------------------------
+
+
+# Make all files readable; directories readable and executable
+chmod a+rxs $MODULES_DIR/$MODULE_NAME
+find $MODULES_DIR/$MODULE_NAME/$VERSION -type d -print0 | xargs -0 chmod a+rxs
+find $MODULES_DIR/$MODULE_NAME/$VERSION -type f -print0 | xargs -0 chmod a+r
+chmod a+rxs $MODULESFILES_DIR/$MODULE_NAME
+find $MODULESFILES_DIR/$MODULE_NAME -type f -print0 | xargs -0 chmod a+r
+
+# Make all files, that were already executable for the user, readable and executable for all
+find $MODULES_DIR/$MODULE_NAME/$VERSION -type f -executable -print0 | xargs -0 chmod a+rx
+
+
+
